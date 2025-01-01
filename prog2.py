927d4b1805200ea5a88fd809d09f6a42cf7a1f6b
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2025-01-01 22:43:01 +0530

    Update prog2.py

--- prog2.py
+++ prog2.py
@@ -4,6 +4,7 @@ from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfTransformer
 
 # Load the IMDB dataset
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=10000)
@@ -13,7 +14,6 @@ X_train = pad_sequences(X_train, maxlen=200)
 X_test = pad_sequences(X_test, maxlen=200)
 
 # Convert the sequences to TF-IDF features
-from sklearn.feature_extraction.text import TfidfTransformer
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train)
 X_test_tfidf = tfidf_transformer.transform(X_test)
