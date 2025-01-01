0bf43bf68822a016db572a04bc6ba403bc3596bb
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2025-01-01 23:24:15 +0530

    Update prog4.py

--- prog4.py
+++ prog4.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
-import matplotlib.pyplot as plt
 import numpy as np
 
 # Load dataset
